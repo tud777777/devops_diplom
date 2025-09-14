21f772c141c58eb8908b1a54fad62bfbeb20012f
tud777777 <66837353+tud777777@users.noreply.github.com>
2025-09-14 03:47:02 +0300

    Update providers.tf

--- S3-bucket and account/providers.tf
+++ S3-bucket and account/providers.tf
@@ -12,6 +12,7 @@ terraform {
 provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
-  service_account_key_file = file("../secrets/user_key.json")
+  service_account_key_file = file("~/devops_diplom/main/authorized_keys/authorized_key_file.json")
   zone                     = var.default_zone
+
 }
